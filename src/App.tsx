489ef45aa30365f2412527a7ea5b02063365dd79
Faridah Salami <f.salami@outlook.com>
2025-11-24 16:05:22 +0000

    Added cards for clearer expectation of lesson plan

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
-import DailyWisdom from "./pages/DailyWisdom";
 import STEAMExplorerPage from "./pages/STEAMExplorerPage";
 import AlBattaniPage from "./pages/AlBattaniPage";
 import JabirIbnHayyanPage from "./pages/JabirIbnHayyanPage";
@@ -17,13 +16,6 @@ import AbbasIbnFirnasPage from "./pages/AbbasIbnFirnasPage";
 import AlJazariPage from "./pages/AlJazariPage";
 import LessonGeneratorPage from "./pages/LessonGeneratorPage";
 import LessonPlanDisplay from "./pages/LessonPlanDisplay";
-import SavedLessons from "./pages/SavedLessons";
-import Community from "./pages/Community";
-import Auth from "./pages/Auth";
-import Dashboard from "./pages/Dashboard";
-import AmbassadorDashboard from "./pages/AmbassadorDashboard";
-import AmbassadorApplication from "./pages/AmbassadorApplication";
-import AdminDashboard from "./pages/AdminDashboard";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -38,7 +30,6 @@ const App = () => (
           <Routes>
             <Route path="/" element={<LessonGeneratorPage />} />
             <Route path="/about" element={<Index />} />
-            <Route path="/daily-wisdom" element={<DailyWisdom />} />
             <Route path="/steam-explorer" element={<STEAMExplorerPage />} />
             <Route path="/scientist/al-battani" element={<AlBattaniPage />} />
             <Route path="/scientist/jabir-ibn-hayyan" element={<JabirIbnHayyanPage />} />
@@ -50,13 +41,6 @@ const App = () => (
             <Route path="/scientist/al-jazari" element={<AlJazariPage />} />
             <Route path="/lesson-generator" element={<LessonGeneratorPage />} />
             <Route path="/lesson-plan" element={<LessonPlanDisplay />} />
-            <Route path="/saved-lessons" element={<SavedLessons />} />
-            <Route path="/community" element={<Community />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/ambassador-dashboard" element={<AmbassadorDashboard />} />
-            <Route path="/ambassador-application" element={<AmbassadorApplication />} />
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
